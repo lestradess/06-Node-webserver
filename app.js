2cc7b229385ecc050eacaa16c53ae00bd353284d
Lestrades_ubu <Lestrades@gmail.com>
2022-11-08 19:09:39 +0100

    Servir contenido estático

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+//Notas middleware de express || Servir contenido estatico
+app.use(express.static('public'));
+
 //Notas Entra en la pagina principal
+//Notas Cuando se ejecuta el middleware esta linea de abajo no se ejecuta
 app.get('/', (req, res) => {
     res.send('Página principal');
 });
@@ -11,8 +15,12 @@ app.get('/hola-mundo', (req, res) => {
     res.send('Página Hola mundo');
 });
 //Notas Entra si no es ninguna de las otras alternativas
+// app.get('*', (req, res) => {
+//     res.send('404| page no found');
+// });
+//Notas Para mandar contenido de otra carpeta
 app.get('*', (req, res) => {
-    res.send('404| page no found');
+    res.sendFile(__dirname + '/public/404.html');
 });
 //Notas Describe por consola la informacion
 app.listen(port, () => {
