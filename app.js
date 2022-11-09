dcc0b466eaeb2f6b9af69310d791922ae3065f7d
Lestrades_ubu <Lestrades@gmail.com>
2022-11-09 17:27:56 +0100

    Utilizando Handlebars para Express

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express();
 const port = 8080;
 const ruta = 'public/template-roadtrip/';
 
+//Notas Esto es requido para utilizar hbs con Express
+app.set('view engine', 'hbs');
+
 //Notas middleware de express || Servir contenido estatico
-app.use(express.static('public/template-roadtrip/'));
+app.use(express.static('public'));
 
 //Notas Para mandar contenido de otra carpeta
+app.get('/', (req, res) => {
+    res.render('home');
+});
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname +'/'+ ruta +'/generic.html');
 });
